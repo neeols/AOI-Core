28dfabbf07ba940b914dde5888deaa0c73aa4033
neeols <101673369+neeols@users.noreply.github.com>
2023-06-09 19:39:24 -0700

    Added Files

---
+++ src/main/java/ageofingenuity/aoiValues.java
@@ -0,0 +1,8 @@
+package ageofingenuity;
+
+import gregtech.api.unification.material.MarkerMaterials.*;
+import gregtech.api.unification.material.Material;
+
+public class aoiValues {
+    public static Material[] TierMaterials = new Material[] { Tier.ULV, Tier.LV, Tier.MV, Tier.HV, Tier.EV, Tier.IV, Tier.LuV, Tier.ZPM, Tier.UV, Tier.UHV, Tier.UEV, Tier.UIV, Tier.UXV, Tier.OpV, Tier.MAX };
+}
